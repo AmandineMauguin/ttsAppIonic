7da34e7c6035698b81ae47667ee4487466de06e5
Amandine Mauguin <greenies@mbp-de-amandine-1.home>
2021-02-03 16:52:53 +0100

    settings et btn clear, stop

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+
 import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
+import { Clipboard } from '@ionic-native/clipboard/ngx';
+
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab1',
@@ -7,12 +11,64 @@ import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  constructor(private tts: TextToSpeech) {}
+  locale = 'fr-FR';
+  rate = '2';
+  
+  constructor(
+    private tts: TextToSpeech,
+    private clipboard: Clipboard,
+    private storage: Storage
+  ) {}
 
+  ionViewWillEnter() {
+    this.storage.get('locale').then((val) => {
+      console.log('La valeur de local :', val);
+      this.locale = val ?? val; //Puis on test si la valeur contient bien quelque chose
+    });
+    this.storage.get('rate').then((val) => {
+      console.log('La valeur de rate :', val);
+      if (val) this.rate = val; //Puis on test si la valeur contient bien quelque chose
+    });
+  }
+  saveLocale(event) {
+    this.storage.set('locale', this.locale);
+  }
+
+  saveRate(event) {
+    this.storage.set('rate', this.rate);
+    // console.log(event.target.value);
+  }
+
+  text2Speech:string;
   speak() {
     this.tts
-      .speak('Hello World')
+      .speak({
+        text: this.text2Speech,
+        locale: 'fr-FR',
+        rate: 2,
+      })
       .then(() => console.log('Success'))
-      .catch((reason: any) => console.log('TSS ERROR : ',reason));
+      .catch((reason: any) => console.log('TSS ERROR : ', reason));
+  }
+
+  stop() {
+    this.tts
+      .speak('')
+      .then(() => console.log('Success'))
+      .catch((reason: any) => console.log(reason));
+  }
+
+  pasteClip() {
+    this.clipboard.paste().then(
+      (resolve: string) => {
+        alert(resolve);
+      },
+      (reject: string) => {
+        alert('Clipboard error: ' + reject);
+      }
+    );
+  }
+  clear() {
+    this.text2Speech = '';
   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  locale = 'fr-FR';
+  rate = '2';
 
-  constructor() {}
+  constructor(private storage: Storage) {}
 
+  saveLocale(event) {
+    this.storage.set('locale', this.locale);
+  }
+
+  saveRate(event) {
+    this.storage.set('rate', this.rate);
+    // console.log(event.target.value);
+  }
+
+  ionViewWillEnter() {
+    this.storage.get('locale').then((val) => {
+      console.log('La valeur de local :', val);
+      this.locale = val ?? val; //Puis on test si la valeur contient bien quelque chose
+    });
+    this.storage.get('rate').then((val) => {
+      console.log('La valeur de rate :', val);
+      if (val) this.rate = val; //Puis on test si la valeur contient bien quelque chose
+    });
+  }
 }
